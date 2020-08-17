adde9c162608147aae6650f43eae7b5aaf2ff761
Aviv5374 <aviv5374@gmail.com>
2020-08-17 09:27:34 +0300

    Lerning The New InputSystem 1

---
+++ V3D/Assets/_MyAssets/Sctips/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+    PlayerActionInputs inputActions;
+
+    #region Initialization
+
+	void Awake()
+    {
+        inputActions = new PlayerActionInputs();
+	}
+
+    void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    void Start()
+    {
+        
+    }
+     
+    #endregion
+
+    #region Updating	 
+
+    void Update()
+    {
+        float horizontal = inputActions.Move.Horizontal.ReadValue<float>();
+        Debug.Log(horizontal);
+        //Vector3 move = new Vector3(0, 0, 0);
+        //transform.Translate(move * speed * Time.deltaTime);
+    }
+
+    #endregion
+
+    void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+}
