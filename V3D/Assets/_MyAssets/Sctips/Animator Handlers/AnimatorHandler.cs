89be562b0dcc2f32a63630ba1cfaabc3aae09d7f
Aviv5374 <aviv5374@gmail.com>
2020-08-18 09:52:34 +0300

    Last Touches Of The Third Person Movement System & First Touches On The Animation System

---
+++ V3D/Assets/_MyAssets/Sctips/Animator Handlers/AnimatorHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimatorHandler : MonoBehaviour
+{
+    protected Animator myAnimator;
+    protected List<AnimatorControllerParameter> myAnimtorParameters = new List<AnimatorControllerParameter>();
+
+    public bool IsInIdle
+    {
+        get { return myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle"); }
+    }
+
+    protected virtual void Awake()
+    {
+        SetMyAnimator();
+    }
+
+    void SetMyAnimator()//TODO:FINE Better Method Name
+    {
+        //This suppose to be in Awake and just in case also in a property
+        if (!myAnimator)
+        {
+            myAnimator = GetComponent<Animator>();
+        }
+        //TODO: If myAnimator is still null after GetComponent<Animator>() I am Fuck. Fix it.
+
+        //This suppose to be in Start 
+        for (int index = 0; index < myAnimator.parameterCount; index++)
+        {
+            myAnimtorParameters.Add(myAnimator.GetParameter(index));
+        }
+    }
+
+    public virtual void AnimatorControllerSwicher(AnimatorOverrideController animatorOverride)
+    {
+        var overrideController = myAnimator.runtimeAnimatorController as AnimatorOverrideController;
+        if (animatorOverride)
+        {
+            myAnimator.runtimeAnimatorController = animatorOverride;
+        }
+        else if (overrideController)
+        {
+            myAnimator.runtimeAnimatorController = overrideController.runtimeAnimatorController;
+        }
+
+    }
+
+    //TODO:Maybe private Update is Better? DO i need the Update method????
+    //protected virtual void Update()
+    //{
+    //    if (IsInIdle)
+    //    {
+    //        return;
+    //    }
+    //}
+}
---
+++ V3D/Assets/_MyAssets/Sctips/Animator Handlers/PlayerAnimatorHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatorHandler : AnimatorHandler
+{   
+
+    #region Initialization
+
+	void Awake()
+	{
+		
+	}
+			
+    void Start()
+    {
+        
+    }
+     
+    #endregion
+
+    #region Updating	 
+
+    void Update()
+    {
+        
+    }
+	
+	#endregion
+		
+}
