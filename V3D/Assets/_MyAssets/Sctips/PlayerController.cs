f5c4888889521a30bcdeaa90afcdcc3798400bdd
Aviv5374 <aviv5374@gmail.com>
2020-08-18 09:02:00 +0300

    Returning To The Old Input System & Cleaning Code

--- V3D/Assets/_MyAssets/Sctips/PlayerController.cs
+++ V3D/Assets/_MyAssets/Sctips/PlayerController.cs
@@ -1,31 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 5f;
-
-    float _gravity = 9.81f;
-
-    PlayerActionInputs inputActions;
     CharacterController controller;
     ThirdPersonController thirdPersonController;
 
     #region Initialization
 
     void Awake()
-    {
-        inputActions = new PlayerActionInputs();
+    {        
         controller = GetComponent<CharacterController>();
         thirdPersonController = GetComponent<ThirdPersonController>();
 	}
-
-    void OnEnable()
-    {
-        inputActions.Enable();
-    }
+    
 
     void Start()
     {
@@ -44,32 +33,13 @@ public class PlayerController : MonoBehaviour
     #endregion
 
     void Move()
-    {
-        //float horizontalAxis = inputActions.Move.Horizontal.ReadValue<float>();
-        //float verticalAxis = inputActions.Move.Vertical.ReadValue<float>();
+    {        
         float horizontalAxis = Input.GetAxisRaw("Horizontal");
         float verticalAxis = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontalAxis, 0, verticalAxis).normalized;
         thirdPersonController.ThirdPersonMovment(direction);
     }
 
-    void CalculateMovement()
-    {
-        float horizontalAxis = inputActions.Move.Horizontal.ReadValue<float>();
-        float verticalAxis = inputActions.Move.Vertical.ReadValue<float>();
-        //Debug.Log("horizontal Axis=" + horizontalAxis);
-        //Debug.Log("vertical Axis=" + verticalAxis);
-        Vector3 direction = new Vector3(horizontalAxis, 0, verticalAxis).normalized;
-        Debug.Log("direction.magnitude = " + direction.magnitude);
-        //Vector3 velocity = direction * speed;
-        //velocity.y -= _gravity;
-        //velocity = transform.TransformDirection(velocity);
-        //controller.Move(velocity * Time.deltaTime);
-    }
-
-    void OnDisable()
-    {
-        inputActions.Disable();
-    }
+        
 
 }
